cc9cc26e0e4f11c374a4448d208afce3217829d0
maxmarumbwa <maxmarumbwa@gmail.com>
2025-05-21 23:05:49 +0200

    api raw data on home.html

--- lookup/views.py
+++ lookup/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 
 # Createf your views here.
 def home(request):
-    return render(request, "home.html", {})
+    import json
+    import requests
+
+    api_request = requests.get(
+        "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=5269525E-87B5-47A7-875C-C8D9FA5278A9"
+    )
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, "home.html", {"api": api})
 
 
 def about(request):
