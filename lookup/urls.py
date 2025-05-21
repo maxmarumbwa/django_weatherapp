f98e999b8827d02a7651cfeb601349bae1ad2f6d
maxmarumbwa <maxmarumbwa@gmail.com>
2025-05-21 20:55:49 +0200

    initial commit homepage loading

---
+++ lookup/urls.py
@@ -0,0 +1,8 @@
+# This is my views.py file
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+]
